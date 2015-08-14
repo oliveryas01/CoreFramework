6a5714b1e68093071443bfcaab7c4dadeb4235af
BuildTools <oliveryas01@gmail.com>
2015-08-13 23:07:17 -0400

    Added more colors.

--- src/main/java/com/coreframework/gui/color/Color.java
+++ src/main/java/com/coreframework/gui/color/Color.java
@@ -15,13 +15,25 @@ public final class Color
 	public static final Color gold = new Color(255, 170, 0); // §6
 	public static final Color gray = new Color(170, 170, 170); // §7
 	public static final Color darkGray = new Color(85, 85, 85); // §8
-	public static final Color BLUE = new Color(85, 85, 255); // §9
-	public static final Color GREEN = new Color(85, 255, 85); // §a
+	public static final Color blue = new Color(85, 85, 255); // §9
+	public static final Color green = new Color(85, 255, 85); // §a
 	public static final Color aqua = new Color(85, 255, 255); // §b
-	public static final Color RED = new Color(255, 85, 85); // §c
+	public static final Color red = new Color(255, 85, 85); // §c
 	public static final Color lightPurple = new Color(255, 85, 255); // §d
 	public static final Color yellow = new Color(255, 255, 85); // §e
 	public static final Color white = new Color(255, 255, 255); // §f
+
+	// Main colors.
+	public static final Color RED = new Color(255, 0, 0);
+	public static final Color GREEN = new Color(0, 255, 0);
+	public static final Color BLUE = new Color(0, 0, 255);
+	public static final Color YELLOW = new Color(255, 255, 0);
+	public static final Color AQUA = new Color(0, 255, 255);
+	public static final Color PURPLE = new Color(255, 0, 255);
+	public static final Color GRAY = new Color(192, 192, 192);
+
+	// Styling colors.
+	public static final Color transparent = new Color(0, 0, 0, 0);
 
 	public Color(final int red, final int green, final int blue, final int alpha)
 	{
@@ -30,9 +42,9 @@ public final class Color
 		if(blue < 0 || blue > 255) throw new IllegalArgumentException("The blue value is out of bounds (0-255).");
 		if(alpha < 0 || alpha > 255) throw new IllegalArgumentException("The alpha value is out of bounds (0-255).");
 
-		this.red = red;
-		this.green = green;
-		this.blue = blue;
+		r = red;
+		g = green;
+		b = blue;
 		this.alpha = alpha;
 	}
 
@@ -41,11 +53,11 @@ public final class Color
 		this(red, green, blue, 255);
 	}
 
-	private int red;
+	private int r;
 
-	private int green;
+	private int g;
 
-	private int blue;
+	private int b;
 
 	private int alpha;
 
@@ -53,55 +65,55 @@ public final class Color
 	{
 		return
 				((alpha & 0xff) << 24) |
-				((red & 0xff) << 16) |
-				((green & 0xff) << 8) |
-				((blue & 0xff) << 0);
+				((r & 0xff) << 16) |
+				((g & 0xff) << 8) |
+				((b & 0xff) << 0);
 	}
 
 	public void setColor(final java.awt.Color color)
 	{
-		red = color.getRed();
-		green = color.getGreen();
-		blue = color.getBlue();
+		r = color.getRed();
+		g = color.getGreen();
+		b = color.getBlue();
 		alpha = color.getAlpha();
 	}
 
 	public void setColor(final org.lwjgl.util.Color color)
 	{
-		red = color.getRed();
-		green = color.getGreen();
-		blue = color.getBlue();
+		r = color.getRed();
+		g = color.getGreen();
+		b = color.getBlue();
 		alpha = color.getAlpha();
 	}
 
 	public int getRed()
 	{
-		return red;
+		return r;
 	}
 
 	public void setRed(int red)
 	{
-		this.red = red;
+		this.r = red;
 	}
 
 	public int getGreen()
 	{
-		return green;
+		return g;
 	}
 
 	public void setGreen(int green)
 	{
-		this.green = green;
+		this.g = green;
 	}
 
 	public int getBlue()
 	{
-		return blue;
+		return b;
 	}
 
 	public void setBlue(int blue)
 	{
-		this.blue = blue;
+		this.b = blue;
 	}
 
 	public int getAlpha()
